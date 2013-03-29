4fd953f0dcab73ba4b5bae1103dbd24517d2c6ae
Pampipampupampa <jeremybois@rocketmail.com>
2013-03-29 20:54:03 +0100

    Cours 29/03 avant manger 2

---
+++ Livre/Tkinter/calculette.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+# -*- coding:Utf8 -*-
+
+
+" CREATION D'UNE CALCULETTE"
+
+
+############# Importation fonction et modules : ################
+
+
+from math import *
+from tkinter import *
+
+
+############# Gestion d'évènements : définition de différentes fonctions utiliées  : ##############
+
+
+def evaluer(event):
+	"Permet de modifier la variable chaine afin d'ajouter le réultat du calcul"
+	chaine.configure(text = "Résultat = " + str(eval(entree.get())))
+
+
+############## Programme principal : #################
+
+
+fenetre = Tk()
+entree = Entry(fenetre) # ajoute un champs dans la fenêtre où l'utilisateur peut rentrer la formule
+entree.bind("<Return>", evaluer) # création d'un raccourci pour finaliser le calcul
+chaine = Label(fenetre) # ajoute un emplacement pour du texte sur l'interface
+entree.pack()
+chaine.pack()
+fenetre.mainloop()
+
